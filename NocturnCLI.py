915d0fde02f46c2e702e1f10b26ba7bc199a0b60
ramstorm <david.ramstrom@gmail.com>
2023-10-06 22:22:19 +0200

    Added NocturnCLI to run without wx UI

---
+++ NocturnCLI.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python2
+
+import threading
+import sys
+import signal
+
+from pubsub import pub
+from Configurator import YAMLConfigurator
+import Midder
+from NocturnModel import NocturnModel
+from optparse import OptionParser
+
+
+class CLINocturnModel( NocturnModel ):
+    def notifyObservers( self ):
+        self.update()
+        for obs in self.observers:
+            obs.notify()
+
+class PollThread( threading.Thread ):
+    
+    def __init__( self, configFile ):
+        super( PollThread, self ).__init__()
+        config = YAMLConfigurator( configFile )
+        
+        self.nocturn = CLINocturnModel()
+        self.midder = Midder.getMidder()
+        config.fileToProg( self.nocturn )
+        
+        self._stop = False
+        
+        #self.start()
+    
+    def run( self ):        
+        
+        while( not self._stop ):
+            self.nocturn.poll()
+            midi = self.midder.recv()
+            if midi:
+                pub.sendMessage( 'MIDI_IN_MESSAGES', channel=midi[0],
+                        cc=midi[1], value=midi[2] )
+    
+    def stop( self ):
+        self._stop = True
+    
+    def getNocturn( self ):
+        return self.nocturn
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filename",
+                      help="configuration file", metavar="FILE")
+    (options, args) = parser.parse_args()
+    try:
+        thread = PollThread( options.filename )
+        thread.start()
+        signal.pause()
+    except KeyboardInterrupt:
+        print "KBInterrupt received, exiting..."
+        thread.stop()
+        sys.exit()
+
